54afd1b5c0bde8c48a349c58cd5525ed3e3b0fdc
XX-Terminator <93933013+XX-Terminator@users.noreply.github.com>
2023-11-27 15:11:38 +0100

    Update host.py

--- 3_En_Ralla/host.py
+++ 3_En_Ralla/host.py
@@ -1,21 +1,17 @@
 import socket 
 import pickle 
+from Definiciones_3_En_Ralla import Tres_En_Ralla                               #Importamos el juego
 
-from Definiciones_3_En_Ralla import Tres_En_Ralla                                #Importamos el juego
+HOST = '127.0.0.1'                                                              #Dirección IP del localhost
+PORT = 1024                                                                     #Puerto   
 
-HOST = '127.0.0.1'                                                      #Dirección IP del localhost
-PORT = 1024                                                            #Puerto   
-
-#servidor
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                   #Tipo de direción IP: AF_INET = IPv4; protocolo socket: SOCK_STREAM = TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                           #Montamos el servidor. Tipo de direción IP: AF_INET = IPv4; protocolo socket: SOCK_STREAM = TCP
 s.bind((HOST, PORT))                                            
-s.listen(5)
-                                            
-socket_client, direccion_cliente = s.accept()                           #aceptamos la conexión con el cliente
+s.listen(5)                                    
+socket_client, direccion_cliente = s.accept()                                   #aceptamos la conexión con el cliente
 print(f"\nConnected to {direccion_cliente}!")
-
 jugador = Tres_En_Ralla("X")
-play = True                                                             #Esta variable nos permite jugar
+play = True                                                                     #Esta variable nos permite jugar
 while play == True:
 
     jugador.welcoming()
@@ -24,22 +20,20 @@ while play == True:
         jugador.print_turno()
         jugador.tablero()
         jugador.colocarFixa()
-        jugador.tablero()                                               #Dibujamos el tablero 
+        jugador.tablero()                                                          #Dibujamos el tablero 
+        act_fichas_tablero = pickle.dumps(jugador.Fichas_en_Tablero)               #selecciona la lista de fichas en el tablero para convertirla en una secuencia de bytes, para poder enviarla
+        socket_client.send(act_fichas_tablero)                                     #enviamos al host la informacion
 
-        simbolX = pickle.dumps(jugador.Fichas_en_Tablero)               #selecciona la lista de fichas en el tablero para convertirla en una secuencia de bytes, para poder enviarla
-        socket_client.send(simbolX)                                     #enviamos al host la informacion
-
-        if jugador.ganar("X") == True or jugador.empate() == True:      #si hay empate o victoria salimos del bucle, porque se ha acabado el juego  
+        if jugador.ganar("X") == True or jugador.empate() == True:                  #si hay empate o victoria salimos del bucle, porque se ha acabado el juego  
           break
 
         print(f"\nWaiting for the rival to move his token...")
-        simbolO = socket_client.recv(1024)
-        simbolO = pickle.loads(simbolO)
-        jugador.actualitza_Tablero(simbolO)
+        act_fichas_tablero = socket_client.recv(2048)
+        act_fichas_tablero = pickle.loads(act_fichas_tablero)
+        jugador.actualitza_Tablero(act_fichas_tablero)
     
     jugador.tablero()
     jugador.print_resultado()
-
     venganza = jugador.input_cont_jugando()                         
     send_venganza = pickle.dumps(venganza)                        
     socket_client.send(send_venganza)
@@ -49,9 +43,8 @@ while play == True:
         play = False
 
     else:
-     
         print(f"Waiting for the rival's response...")
-        venganza_rival = socket_client.recv(1024)
+        venganza_rival = socket_client.recv(2048)
         venganza_rival = pickle.loads(venganza_rival)
  
         if venganza_rival == "No":
@@ -60,8 +53,5 @@ while play == True:
         elif venganza_rival =="Yes":
             jugador.reiniciar_juego()
         
-
-
 enter = input(f"\nThank You For Playing!!\nPress ENTER to skip...\n")
-
 socket_client.close()
